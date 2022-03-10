c4af5ab82ca7946a20c0093a3e4511ac99a6ec56
Kaladin13 <335095@niuitmo.ru>
2022-03-10 19:27:44 +0300

    padding

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -6,7 +6,8 @@ import {
     wallet,
     tz
 } from "../../utils/wallet";
-import { Box, Button, Container, FormControl, IconButton, InputAdornment, InputLabel, MenuItem, OutlinedInput,
+import {
+    Box, Button, Container, FormControl, IconButton, InputAdornment, InputLabel, MenuItem, OutlinedInput,
     Select, SelectChangeEvent
 } from "@mui/material";
 import CompareArrowsOutlinedIcon from '@mui/icons-material/CompareArrowsOutlined';
@@ -72,93 +73,93 @@ function Transaction(props) {
         // trader, left untouched, fixed window
 
         <body className="h-full bg-transparent flex items-center justify-center">
-        <div className="m-10"></div>
-        <div className="absolute insert-0 top-10 m-40 h-fit w-96 bg-white p-8 shadow-lg rounded-lg">
-            <div className="text-black">
-                <div className="pb-5 text-center font-bold">TRADER</div>
-                <div className="mb-5">
-                <FormControl fullWidth sx={{ m: 0 }}>
-                                <InputLabel htmlFor="outlined-adornment-amount">From</InputLabel>
-                                <OutlinedInput
-                                    id="outlined-adornment-amount"
-                                    value={currencyFromNumber}
-                                    onChange={handleChangeFromNumber}
-                                    type="number"
-                                    placeholder="0.0"
-                                    startAdornment={
-                                        <InputAdornment position="start">
-                                            <Select
-                                                variant="standard"
-                                                onChange={onFromSelectChange}
-                                                value={currencyFrom}
-                                            >
-                                                <MenuItem value='tez'>
-                                                    XTZ
-                                                </MenuItem>
-                                                <MenuItem value='USD'>
-                                                    USD
-                                                </MenuItem>
-                                                <MenuItem value={KOLIBRI_TOKEN_ADDRESS}>
-                                                    kUSD
-                                                </MenuItem>
-                                            </Select>
-                                        </InputAdornment>}
-                                    label="Amount"
-                                />
-                            </FormControl>
-                        </div>
+            <div className="m-10"></div>
+            <div className="absolute insert-0 top-10 m-40 h-fit w-96 bg-white p-8 shadow-lg rounded-lg">
+                <div className="text-black">
+                    <div className="pb-5 text-center font-bold">TRADER</div>
+                    <div className="mb-5">
+                        <FormControl fullWidth sx={{ m: 0 }}>
+                            <InputLabel htmlFor="outlined-adornment-amount">From</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-amount"
+                                value={currencyFromNumber}
+                                onChange={handleChangeFromNumber}
+                                type="number"
+                                placeholder="0.0"
+                                startAdornment={
+                                    <InputAdornment position="start">
+                                        <Select
+                                            variant="standard"
+                                            onChange={onFromSelectChange}
+                                            value={currencyFrom}
+                                        >
+                                            <MenuItem value='tez'>
+                                                XTZ
+                                            </MenuItem>
+                                            <MenuItem value='USD'>
+                                                USD
+                                            </MenuItem>
+                                            <MenuItem value={KOLIBRI_TOKEN_ADDRESS}>
+                                                kUSD
+                                            </MenuItem>
+                                        </Select>
+                                    </InputAdornment>}
+                                label="Amount"
+                            />
+                        </FormControl>
+                    </div>
 
-                        <div className="flex flex-col items-center border-black">
-                            <p>
-                                Rate: 1.3%
-                            </p>
-                            <IconButton
-                                style={{ height: "45px", width: "45px" }}
-                                onClick={handleChangeCurrencies}>
-                                <CompareArrowsOutlinedIcon />
-                            </IconButton>
-                        </div>
+                    <div className="flex flex-col items-center border-black">
+                        <p>
+                            Rate: 1.3%
+                        </p>
+                        <IconButton
+                            style={{ height: "45px", width: "45px" }}
+                            onClick={handleChangeCurrencies}>
+                            <CompareArrowsOutlinedIcon />
+                        </IconButton>
+                    </div>
 
-                        <div>
-                            <FormControl fullWidth sx={{ m: 0 }}>
-                                <InputLabel htmlFor="outlined-adornment-amount">To</InputLabel>
-                                <OutlinedInput
-                                    id="outlined-adornment-amount"
-                                    value={currencyToNumber}
-                                    onChange={handleChangeToNumber}
-                                    type="number"
-                                    placeholder="0.0"
-                                    startAdornment={
-                                        <InputAdornment position="start">
-                                            <Select
-                                                variant="standard"
-                                                onChange={onToSelectChange}
-                                                value={currencyTo}
-                                            >
-                                                <MenuItem value='tez'>
-                                                    XTZ
-                                                </MenuItem>
-                                                <MenuItem value='USD'>
-                                                    USD
-                                                </MenuItem>
-                                                <MenuItem value={KOLIBRI_TOKEN_ADDRESS}>
-                                                    kUSD
-                                                </MenuItem>
-                                            </Select>
-                                        </InputAdornment>}
-                                    label="Amount"
-                                />
-                            </FormControl>
-                            <div className="pt-4 flex items-center justify-center">
+                    <div>
+                        <FormControl fullWidth sx={{ m: 0 }}>
+                            <InputLabel htmlFor="outlined-adornment-amount">To</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-amount"
+                                value={currencyToNumber}
+                                onChange={handleChangeToNumber}
+                                type="number"
+                                placeholder="0.0"
+                                startAdornment={
+                                    <InputAdornment position="start">
+                                        <Select
+                                            variant="standard"
+                                            onChange={onToSelectChange}
+                                            value={currencyTo}
+                                        >
+                                            <MenuItem value='tez'>
+                                                XTZ
+                                            </MenuItem>
+                                            <MenuItem value='USD'>
+                                                USD
+                                            </MenuItem>
+                                            <MenuItem value={KOLIBRI_TOKEN_ADDRESS}>
+                                                kUSD
+                                            </MenuItem>
+                                        </Select>
+                                    </InputAdornment>}
+                                label="Amount"
+                            />
+                        </FormControl>
+                        <div className="pt-4 flex items-center justify-center">
                             <button
                                 onClick={handleSwapToken}
                                 className="m-4 w-40 bg-blue-600 hover:bg-blue-700 p-2 text-white rounded-lg shadow-lg">
-                                    Swap
+                                Swap
                             </button>
-                            </div>
                         </div>
                     </div>
-              </div>
+                </div>
+            </div>
 
 
 
@@ -167,10 +168,10 @@ function Transaction(props) {
 
 
 
-          {/* footer, why not in wallet?*/}
-         <Footer/>  
+            {/* footer, why not in wallet?*/}
+            <Footer />
 
-    </body>  
+        </body>
 
     )
 }
