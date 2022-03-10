f8a9ccb6355867edb9d8c3347311205869a72534
Kaladin13 <335095@niuitmo.ru>
2022-03-10 19:22:48 +0300

    Split footer and add comments

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -14,6 +14,7 @@ import { React, useEffect, useState } from "react";
 import { estimateOutput, getBalanceKolibri } from '../../utils/kolibri';
 import { KOLIBRI_TOKEN_ADDRESS } from "../../utils/values";
 import { swapToken } from "../../utils/swap";
+import Footer from "../Footer/Footer";
 
 function Transaction(props) {
 
@@ -66,6 +67,10 @@ function Transaction(props) {
     }, [TESTNET]);
 
     return (
+
+
+        // trader, left untouched, fixed window
+
         <body className="h-full bg-transparent flex items-center justify-center">
         <div className="m-10"></div>
         <div className="absolute insert-0 top-10 m-40 h-fit w-96 bg-white p-8 shadow-lg rounded-lg">
@@ -154,32 +159,17 @@ function Transaction(props) {
                         </div>
                     </div>
               </div>
-          {/* footer */}
-         <footer className="absolute inset-x-0 bottom-0  p-4 bg-transparent rounded-lg shadow dark:bg-gray-800">
-            <div className="sm:flex sm:items-center sm:justify-between">
-                <a href="#" class="flex items-center mb-4 sm:mb-0">
-                    <img src="https://kolibri.finance/img/kolibri-brand.b0cd3374.png" class="mr-3 h-8" alt="Kolibri Logo" />
-                </a>
-                <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-                    <li>
-                        <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">Kolibri™</a>. All Rights Reserved.
-            </span>
-           
-        </footer>   
+
+
+
+
+
+
+
+
+          {/* footer, why not in wallet?*/}
+         <Footer/>  
+
     </body>  
 
     )
